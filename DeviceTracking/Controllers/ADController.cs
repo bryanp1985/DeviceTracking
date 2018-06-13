b63625f3955ed9823d05c8088495b32ac7024aa4
Bryan Perez <bperez@omnicomhealthgroup.com>
2018-06-13 12:10:33 -0500

    add controller and webform that calls service

---
+++ DeviceTracking/Controllers/ADController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DeviceTracking.Controllers
+{
+    public class ADController : ApiController
+    {
+        // GET: api/AD
+        public IEnumerable<string> GetAD(string username)
+        {
+            AD adCall = new AD();
+            string[] results = new string[2];
+            var searcher = adCall.GetDirectorySearcher("OU = CriticalMass, DC = cmass, DC = criticalmass, DC = com", "", "");
+            var searchResult = adCall.SearchUserByUserName(searcher, username);
+            if (searchResult.GetDirectoryEntry().Properties["mail"].Value != null)
+                        results[0] = "Email ID : " + searchResult.GetDirectoryEntry().Properties["mail"].Value.ToString();
+            if (searchResult.GetDirectoryEntry().Properties["givenName"].Value != null)
+                        results[1] = "First Name : " + searchResult.GetDirectoryEntry().Properties["givenName"].Value.ToString();
+                return results;
+        }
+
+        // GET: api/AD/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/AD
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/AD/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/AD/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DeviceTrackingWeb/ADForm.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+
+namespace DeviceTrackingWeb
+{
+    public partial class ADForm : System.Web.UI.Page
+    {
+        private RestClient client = new RestClient("http://localhost:63761/api/");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click_Click(object sender, EventArgs e)
+        {
+            RestRequest request = new RestRequest("AD", Method.GET);
+            IRestResponse<List<string>> response = client.Execute<List<string>>(request);
+        }
+    }
+}
